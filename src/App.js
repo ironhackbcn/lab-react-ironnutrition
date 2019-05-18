c96b01c3dab184c5f4505e5b2f10fedfd17d191d
Allyn Alda <Teddy@Om.local>
2019-05-18 16:21:17 +0200

    done search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
+import 'bulma/css/bulma.css';
+import foods from './data/foods.json'
 import logo from './logo.svg';
 import './App.css';
+import FoodBox from './component/FoodBox';
+import FoodForm from './component/FoodForm.js';
+import Search from './component/Search';
 
 class App extends Component {
+
+  state = {
+    food: foods,
+    showForm: false,
+    searched: '',
+    foodList: []
+  }
+
+  renderForm = () => {
+    this.setState({ showForm: !this.state.showForm })
+  }
+
+  addFood = (newFood) => {
+    const foodsCopy = [...this.state.food];
+    foodsCopy.push(newFood);
+
+    this.setState({ food: foodsCopy })
+  }
+
+  handleSearch = searchedVal => {
+    this.setState({ searched: searchedVal})
+  }
+
+  // handleAddtoList = food => {
+  //   const { foodList } = this.state;
+  //   const index = foodList.map(function(food) { return food.name; }).indexOf(food.name);
+  //   if(index === -1) {
+  //     foodList.push(food);
+  //   }
+  //   this.setState({
+  //     foodList: foodList
+  //   })
+  // } 
+
+  renderFood = (foods, searchval) => {
+    const filteredArray = foods.filter( food => {
+      return food.name.toLowerCase().includes(searchval.toLowerCase())
+    })
+    return filteredArray.map((food, index) => <FoodBox key={index} food={food} handleAddtoList={this.handleAddtoList}/> )
+  }
+
   render() {
+
+    const { food, showForm, searched } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Search handleSearch={this.handleSearch} />
+        <button onClick={this.renderForm}>Add new food</button>
+        {
+          showForm ? <FoodForm addTheFood={this.addFood} /> : null
+        }
+        {
+          this.renderFood(food, searched)
+        }
+        
       </div>
     );
   }
