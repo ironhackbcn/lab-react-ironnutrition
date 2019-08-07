3c1f06dae56dd218e05abe8d75df34d53080c79f
Esther Bernal <esther.bernal.lopez@gmail.com>
2019-08-07 19:40:45 +0200

    hasta la 4 acabada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import foods from './data/foods.json';
 import FoodBox from './components/FoodBox.js';
 import './App.css';
 
@@ -8,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <FoodBox foods={foods} />
+        <FoodBox />
       </div>
     );
   }
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,8 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import foods from '../data/foods.json';
 
 class FoodBox extends Component {
   state = {
     showForm :false,
+    foodsArr : foods,
+    showFood: foods,
+    name: '',
+    calories: '',
+    image:''
   }
   
   changeFormStatus = (event) => {
@@ -12,12 +18,57 @@ class FoodBox extends Component {
     })
   }
 
+  handleInputChange = (event) => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+  
+  handleSearch = (event) => {
+    const {value} = event.target;
+    const foodsFiltered = [...this.state.foodsArr];
+    const results= foodsFiltered.filter(food=> food.name.toLowerCase().includes(value.toLowerCase()));
+    console.log(results);     
+
+    this.setState({
+      showFood:results,
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const {name,calories,image, foodsArr, showForm} = this.state;
+    const foodsCopy = [...foodsArr];
+    const newFood = {name, calories, image};
+    foodsCopy.push(newFood)
+
+    this.setState({
+      foodsArr:foodsCopy,
+      showFood:foodsCopy,
+      name: '',
+      calories: '',
+      image:'',
+      search:'',
+      showForm: !showForm,
+    })
+  }
+
   render() {
-    const {showForm} = this.state
+    const {showForm, name, calories, image,search, showFood} = this.state
     return (
       <>
-        
-        {showForm ? <form>
+        <input className="input"
+          type="text" 
+          id="search" 
+          name="search" 
+          placeholder="e.g. Salad" 
+          value={search}
+          onChange = {this.handleSearch}
+        />
+
+        {showForm ? 
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor="name">Name</label>
           <input 
             className="input"
@@ -25,8 +76,8 @@ class FoodBox extends Component {
             id="name" 
             name="name" 
             placeholder="Pasta" 
-            //value={name}
-            //onChange = {this.handleInputChange}
+            value={name}
+            onChange = {this.handleInputChange}
           />
 
           <label htmlFor="calories">Calories</label>
@@ -36,8 +87,8 @@ class FoodBox extends Component {
             id="calories" 
             name="calories" 
             placeholder="120" 
-            //value={calories}
-            //onChange = {this.handleInputChange}
+            value={calories}
+            onChange = {this.handleInputChange}
           />
 
           <label htmlFor="name">Image</label>
@@ -47,8 +98,8 @@ class FoodBox extends Component {
             id="image" 
             name="image" 
             placeholder="http://www.google.com/image-url" 
-            //value={image}
-            //onChange = {this.handleInputChange}
+            value={image}
+            onChange = {this.handleInputChange}
           />
           <button className="button is-info">+ Add</button>
         </form> : null}
@@ -57,7 +108,7 @@ class FoodBox extends Component {
         :<button className="button is-info" onClick={this.changeFormStatus}>Cancel</button>
         }
         <div className="box">
-        {this.props.foods.map( (food, index) => {
+        {showFood.map( (food, index) => {
           return (
             <article className="media" key={index}>
               <div className="media-left">
