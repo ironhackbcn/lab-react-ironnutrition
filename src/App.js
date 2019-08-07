8fefe3a0a3c4ab22e0456da225a1b5fa3cb1d1f6
annacv <21217131+annacv@users.noreply.github.com>
2019-08-07 14:33:22 +0200

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import foods from './data/foods.json'
+import foods from './data/foods.json';
+import FoodBox from './components/FoodBox.js';
 import './App.css';
 
 class App extends Component {
@@ -8,9 +9,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
         </header>
+        <main>
+          <FoodBox />
+        </main>
       </div>
     );
   }
 }
 
 export default App;
+
+
