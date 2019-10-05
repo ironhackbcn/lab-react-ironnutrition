848475c67a0756cbad90a9855c00621bd0463128
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2019-10-05 14:28:08 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import foods from "./data/foods.json";
+import Card from "./components/Card";
 
 class App extends Component {
+  state = {
+    allFood: [...foods]
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-h1">IronNutrition</h1>
+          <input className="App-Input" type="text" placeholder="...Food"></input>
+          {this.state.allFood.map((food, index) => {
+            return (
+              <div key={`${index}`}>
+                <Card name={food.name} />
+              </div>
+            );
+          })}
         </header>
       </div>
     );
