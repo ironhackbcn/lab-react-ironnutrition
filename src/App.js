79232fa41bcd9bbb3fcac1b4e01c9f4c448b5fda
Elena Piaggio <elenapiaggio@gmail.com>
2019-10-15 15:07:34 +0200

    add iteration 2

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import foods from './data/foods.json';
 import FoodBox from './components/FoodBox'
 
+const listFoods = foods;
+console.log(listFoods);
+console.log('*****************');
+
 class App extends Component {
+  state = {
+    foods: listFoods,
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="contenido">
           <h1> IRON NUTRITION</h1>
-          <FoodBox />
+          <div className="foods-app">
+            {
+              this.state.foods.map( (oneFood, index) => {
+                return(
+                  <FoodBox 
+                    key = {index} {...oneFood}
+                  />
+                )
+              })
+            }
+          </div>
         </div>
       </div>
     );
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,43 +1,43 @@
 import React, { Component } from 'react';
 
-class FoodBox extends Component {
-    render() {
-        return (
-            <div className="box">
-                <article className="media">
-                    <div className="media-left">
-                        <figure className="image is-64x64">
-                            <img src="https://i.imgur.com/eTmWoAN.png" alt="imagen" />
-                        </figure>
+function FoodBox(props) {
+const {name, calories, image, quantity} = props;
+
+    return (
+        <div className="box">
+            <article className="media">
+                <div className="media-left">
+                    <figure className="image is-64x64">
+                        <img src={image} alt="imagen" />
+                    </figure>
+                </div>
+                <div className="media-content">
+                    <div className="content">
+                        <p>
+                            <strong>{name}</strong> <br />
+                            <small>{calories}</small>
+                        </p>
                     </div>
-                    <div className="media-content">
-                        <div className="content">
-                            <p>
-                                <strong>Pizza</strong> <br />
-                                <small>400 cal</small>
-                            </p>
+                </div>
+                <div className="media-right">
+                    <div className="field has-addons">
+                        <div className="control">
+                            <input
+                                className="input"
+                                type="number"
+                                value={quantity}
+                            />
                         </div>
-                    </div>
-                    <div className="media-right">
-                        <div className="field has-addons">
-                            <div className="control">
-                                <input
-                                    className="input"
-                                    type="number"
-                                    value="1"
-                                />
-                            </div>
-                            <div className="control">
-                                <button className="button is-info">
-                                    +
+                        <div className="control">
+                            <button className="button is-info">
+                                +
                                 </button>
-                            </div>
                         </div>
                     </div>
-                </article>
-            </div>
-        );
-    }
+                </div>
+            </article>
+        </div>
+    );
 }
 
 export default FoodBox;
